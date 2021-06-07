5c496afec991bdad10a19e18c5d413951b50fdb2
fang <fzp_ch@foxmail.com>
2021-06-08 02:00:50 +0800

    heuristic improve

---
+++ src/main/java/Heuristic1Solution.java
@@ -0,0 +1,38 @@
+import entity.heuristic.HGroup;
+import entity.heuristic.HNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Heuristic1Solution {
+    List<HGroup> solve(List<HNode> nodes) {
+        List<HGroup> groups = new LinkedList<>();
+        List<HNode> unused = new LinkedList<>(nodes);
+        while (unused.size() != 0) {
+            List<HNode> rest = new LinkedList<>();
+            PriorityQueue<HNode> q = new PriorityQueue<>((HNode h1, HNode h2) -> h1.compareTo(h2) ? -1 : 1);
+            HGroup group = new HGroup();
+            unused.forEach(node -> {
+                node.setValues();
+                q.add(node);
+            });
+            while (!q.isEmpty()) {
+                HNode node = q.poll();
+                node.setValues();
+                if (!q.isEmpty() && !node.compareTo(q.peek())) {
+                    q.add(node);
+                    continue;
+                }
+                if (group.check(node)) {
+                    group.add(node);
+                } else {
+                    rest.add(node);
+                }
+            }
+            groups.add(group);
+            unused = rest;
+        }
+        return groups;
+    }
+}
