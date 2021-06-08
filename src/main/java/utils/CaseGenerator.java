631633d70cdf826affe75e7447beb68a5e831cce
fang <fzp_ch@foxmail.com>
2021-06-09 01:47:25 +0800

    case generator improve

--- src/main/java/utils/CaseGenerator.java
+++ src/main/java/utils/CaseGenerator.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class CaseGenerator {
-    void generate(int nodeNumber, int appNumber, int naMax, int anMin, int anMax) throws IOException {
+    void generate(int nodeNumber, int appNumber, int naMax, int anMin, int anMax, String fileName) throws IOException {
         Random random = new Random();
         int allApps = nodeNumber * naMax - appNumber * anMin;
         int anRest = anMax - anMin;
@@ -22,18 +22,22 @@ public class CaseGenerator {
             } else {
                 appSizes[app - 1] = random.nextInt(anRest + 1);
             }
-            appSizes[app - 1] += anMin;
             allApps -= appSizes[app - 1];
-            HashSet<Integer> nodes = new HashSet<>();
-            for (int i = 0; i < appSizes[app - 1]; i++) {
-                int node = random.nextInt(nodeNumber);
-                while (nodes.contains(node) || nas[node].size() >= naMax) {
-                    node = random.nextInt(nodeNumber);
+            appSizes[app - 1] += anMin;
+            List<Integer> pool = new ArrayList<>();
+            for (int i = 0; i < nodeNumber; i++) {
+                if (nas[i].size() < naMax) {
+                    pool.add(i);
                 }
-                nodes.add(node);
             }
-            for (int node : nodes) {
-                nas[node].add(app);
+            if (appSizes[app - 1] > pool.size()) {
+                appNumber = app - 1;
+                break;
+            }
+            for (int i = 0; i < appSizes[app - 1]; i++) {
+                int index = random.nextInt(pool.size());
+                nas[pool.get(index)].add(app);
+                pool.remove(index);
             }
         }
 
@@ -41,19 +45,18 @@ public class CaseGenerator {
         for (int i = 0; i < appNumber; i++) {
             disruptions[i] = random.nextInt(appSizes[i]) + 1;
         }
-        BufferedWriter out = new BufferedWriter(new FileWriter("test1.txt"));
+        BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
         out.write(String.valueOf(nodeNumber));
         out.newLine();
         out.write(String.valueOf(appNumber));
         out.newLine();
+        for (int i = 0; i < appNumber; i++) {
+            out.write(appSizes[i]+","+disruptions[i]);
+            out.newLine();
+        }
         for (List<Integer> na : nas) {
             List<String> l = na.stream().map(String::valueOf).collect(Collectors.toList());
             out.write(String.join(",", l));
-            out.newLine();
-        }
-
-        for (int i = 0; i < appNumber; i++) {
-            out.write(String.valueOf(disruptions[i]));
             out.newLine();
         }
         out.close();
@@ -61,6 +64,12 @@ public class CaseGenerator {
 
     public static void main(String[] args) throws IOException {
         CaseGenerator gen = new CaseGenerator();
-        gen.generate(4, 3, 3, 2, 4);
+        gen.generate(4, 3, 3, 2, 4, "testCase/test1.txt");
+        for (int i = 2; i <= 10; i++) {
+            gen.generate(500, 400, 5, 1, 20, "testCase/test" + i + ".txt");
+        }
+        for (int i = 11; i <= 20; i++) {
+            gen.generate(5000, 4000, 50, 10, 200, "testCase/test" + i + ".txt");
+        }
     }
 }
